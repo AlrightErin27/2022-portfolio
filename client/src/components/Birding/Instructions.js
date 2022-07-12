42f42007d3149394b2b93226f236cbd79699e5b1
Erin Van Brunt <erinmontybruce@gmail.com>
2022-07-12 14:26:36 -0700

    all games working with scores and user. Just need to save scores to db

--- client/src/components/Birding/Instructions.js
+++ client/src/components/Birding/Instructions.js
@@ -2,15 +2,14 @@ import React from "react";
 import BirdMan from "../../game-imgs/bird-game/binoculars-man.png";
 import Dove from "../../game-imgs/bird-game/dove.png";
 
-function Instructions({ user = "temp-name" }) {
+function Instructions({ user }) {
   return (
     <div className="in-cont">
-      To play try and catch as many birds as you can in 30 seconds.
-      <p>User: {user}</p>
-      <>
+      <p>{user.name}</p>- try to catch as many birds as you can in 30 seconds.
+      <div className="in-img-cont">
         <img src={BirdMan} alt="bird watcher" id="in-watcher" />
         <img src={Dove} alt="dove" id="in-dove" />
-      </>
+      </div>
     </div>
   );
 }
